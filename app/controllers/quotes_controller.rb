3a1fdd2e8c24d5a952edb368370571991e1d3278
yersel500 <yersel500@gmail.com>
2022-09-06 00:48:03 -0500

    setup SPA for Quote model with turbo frames

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
   before_action :set_quote, only: %i[show edit update destroy]
 
   def index
-    @quotes = Quote.all
+    @quotes = Quote.ordered
   end
 
   def show; end
@@ -15,7 +15,10 @@ class QuotesController < ApplicationController
     @quote = Quote.new(quote_params)
 
     if @quote.save
-      redirect_to quotes_path, notice: 'Quote was successfully created.'
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: 'Quote was successfully created.' }
+        format.turbo_stream
+      end
     else
       render :new, status: :unprocessable_entity
     end
