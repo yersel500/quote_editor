49103f58f5f0ae4c9f93d10732c2ecbefbf38ea9
yersel500 <yersel500@gmail.com>
2022-09-05 20:58:23 -0500

    delete quote with turbo_stream

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -8,9 +8,13 @@ class QuotesTest < ApplicationSystemTestCase
     visit quotes_path
     assert_selector 'h1', text: 'Quotes'
     click_on 'New quote'
-    assert_selector 'h1', text: 'New Quote'
     fill_in 'Name', with: 'Capybara quote'
+
+    assert_selector 'h1', text: 'Quotes'
     click_on 'Create quote'
+
+    assert_selector 'h1', text: 'Quotes'
+    assert_text 'Capybara quote'
   end
 
   test 'Showing a quote' do
@@ -25,8 +29,8 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector 'h1', text: 'Quotes'
 
     click_on 'Edit', match: :first
-    assert_selector 'h1', text: 'Edit quote'
     fill_in 'Name', with: 'Updated quote'
+    assert_selector 'h1', text: 'Quotes'
     click_on 'Update quote'
 
     assert_selector 'h1', text: 'Quotes'
