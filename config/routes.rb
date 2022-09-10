3c844c9224869b0f8143017f29f07f6233404e47
yersel500 <yersel500@gmail.com>
2022-09-10 11:22:40 -0500

    add validations, routes,views and styles

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :quotes do
-    resources :line_item_dates, except: %i[index show]
+    resources :line_item_dates, except: %i[index show] do
+      resources :line_items, execept: %i[index show]
+    end
   end
   root 'pages#home'
 end
