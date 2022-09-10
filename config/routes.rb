8a14137a810c2b4bf7c1ab1d146e54cdbf156141
yersel500 <yersel500@gmail.com>
2022-09-10 14:39:17 -0500

    add turbo frames and stream to SPA page and tests

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :quotes do
     resources :line_item_dates, except: %i[index show] do
-      resources :line_items, execept: %i[index show]
+      resources :line_items, except: %i[index show]
     end
   end
   root 'pages#home'
