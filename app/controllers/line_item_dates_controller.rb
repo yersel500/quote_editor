b810a9d676b04a8de1853e3e3243e5bfa1daa9be
yersel500 <yersel500@gmail.com>
2022-09-10 10:26:14 -0500

    add turbo frames and stream to convert to SPA page

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -23,7 +23,11 @@ class LineItemDatesController < ApplicationController
 
   def update
     if @line_item_date.update(line_item_date_params)
-      redirect_to quote_path(@quote), notice: 'Date was succesfully updated.'
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: 'Date was succesfully updated.' }
+        format.turbo_stream { flash.now[:notice] = 'Date was succesfully updated.' }
+      end
+
     else
       render :edit, status: :unprocessable_entity
     end
@@ -31,7 +35,11 @@ class LineItemDatesController < ApplicationController
 
   def destroy
     @line_item_date.destroy
-    redirect_to quote_path(@quote), notice: 'Date was succesfully destroyed.'
+
+    respond_to do |format|
+      format.html { redirect_to quote_path(@quote), notice: 'Date was succesfully destroyed.' }
+      format.turbo_stream { flash.now[:notice] = 'Date was succesfully destroyed.' }
+    end
   end
 
   private
